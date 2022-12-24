2c650e5bd27caaeb2ec22cc7ba22de4eeec0176a
cardenasmarianav.89@gmail.com <cardenasmarianav.89@gmail.com>
2022-12-24 01:21:45 -0300

    actividad de cuadrados y textarea

---
+++ Ejercicio_1.js
@@ -0,0 +1,20 @@
+
+
+function cambiarColorRojo(){
+    let fondo= document.getElementById ("fondo")
+   fondo.style.backgroundColor = "red"
+
+}
+
+
+function cambiarColorVerde(){
+    let fondo= document.getElementById ("fondo")
+   fondo.style.backgroundColor = "green"
+
+}
+
+function cambiarColorAzul(){
+    let fondo= document.getElementById ("fondo")
+   fondo.style.backgroundColor = "blue"
+
+}
---
+++ ejer2.js
@@ -0,0 +1,4 @@
+
+function contar(obj){
+    document.getElementById("contador").innerHTML = obj.value.length+' caracteres';
+}
